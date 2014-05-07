18867b693c638d761bade72b0839c1fecc84a35b
AbstractOwl <abstractOwl@users.noreply.github.com>
2014-05-06 21:53:41 -0700

    Initial commit

---
+++ src/servers/sema/sema.h
@@ -0,0 +1,23 @@
+/**
+ * Header file for the semaphore service.
+ */
+
+#ifndef SEMA_H
+#define SEMA_H
+
+#include <minix/config.h>
+#include <sys/types.h>
+#include <minix/const.h>
+
+#include <minix/syslib.h>
+#include <minix/sysutil.h>
+#include <minix/timers.h>
+
+#include <errno.h>
+
+//#include "proto.h"
+
+//int do_sem_init();
+//int do_sem_down();
+
+#endif
