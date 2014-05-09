ae7faa160c05966cf4cfce1674ccb4507d1b6242
AbstractOwl <abstractOwl@users.noreply.github.com>
2014-05-09 14:21:28 -0700

    Re-add SEF

--- src/servers/sema/sema.c
+++ src/servers/sema/sema.c
@@ -1,22 +1,37 @@
 #include "sema.h"
 
 #define INITIAL_SIZE 10
-#define EXPAND_RATIO 0.5
+#define EXPAND_RATIO 1.5
 
 /**
  * This file contains the main program of a semaphore service implementation
  * for MINIX.
  */
 
-int			*semaphores;
-size_t	sem_len;				// Tracks the current size of the semaphore array
-size_t	tail_pos;				// Tracks the position of the slot after the last
-												//   initialized semaphore
-size_t	min_empty_pos;	// Tracks the minimum open slot
+semaphore_t	*semaphores;
+size_t			sem_len;				// Tracks the current size of the semaphore array
+size_t			tail_pos;				// Tracks the position of the slot after the last
+														//   initialized semaphore
+size_t			min_empty_pos;	// Tracks the minimum open slot
+
+
+// Structs
+typedef struct
+{
+	endpoint_t *value;
+	link_t     *next;
+} link_t;
+
+typedef struct
+{
+	unsigned int value;
+	endpoint_t   *next;
+} semaphore_t;
 
 // Function signatures
 void die(const char *message);
 void log(const char *message);
+int  next_empty_pos();
 
 int do_sem_up				(message *msg);
 int do_sem_down			(message *msg);
@@ -43,6 +58,18 @@ void log(const char *message)
 	fprintf(stdout, "sema.c [INFO ]: %s\n", message);
 }
 
+int next_empty_pos()
+{
+	// There can be no empty slots before min_empty_pos since min_empty_pos is
+	// updated each sem_release
+	int i;
+	for (i = min_empty_pos + 1; i < sem_len; i++) {
+		if (semaphores[i] == NULL) {
+			return i;
+		}
+	}
+}
+
 
 // Semaphore Functions
 /**
@@ -51,7 +78,7 @@ void log(const char *message)
 int init_sem()
 {
 	sem_len				= INITIAL_SIZE;
-	semaphores		= (int *) malloc(sizeof (int) * sem_len);
+	semaphores		= (semaphore_t *) malloc(sizeof (semaphore_t) * sem_len);
 	tail_pos			= 0;
 	min_empty_pos	= 0;
 }
@@ -78,11 +105,23 @@ int do_sem_init(message *msg)
 }
 
 
+struct machine machine;
+
 // sema.c main function
 int main(void)
 {
-	int			result;	// Result to system call
-	message	msg;		// Incoming message
+	endpoint_t	who;			// Caller endpoint
+	message			msg;			// Incoming message
+	int					call_nr;	// System call number
+	int					result;		// Result to system call
+	int					rv;
+	int					s;
+
+	// SEF local startup
+	sef_startup();
+	if ((s = sys_getmachine(&machine)) != OK) {
+		die("Could not get machine info");
+	}
 
 	// Initialize service
 	init_sem();
@@ -91,13 +130,36 @@ int main(void)
 	while (TRUE) {
 		int ipc_status;
 
-		ipc_receive(&msg);
+		// Wait for next message
+		if (sef_receive_status(ANY, &msg, &ipc_status)) {
+			die("sef_receive_status error");
+		}
 
-		switch (msg.m_type) {
+		who				= msg.m_source;	// Sender endpoint
+		call_nr		= msg.m_type;		// System call number
+
+		if (is_ipc_notify(ipc_status)) {
+			switch (who) {
+				case CLOCK:
+					expire_timers(msg.NOTIFY_TIMESTAMP);
+					continue;
+				default:
+					result = ENOSYS;
+			}
+
+			if (result != SUSPEND) {
+				msg.m_type = result;
+				if (send(who, msg) != OK) {
+					LOG("Unable to send reply to endpoint");
+				}
+			}
+		}
+
+		switch (call_nr) {
 		case SEM_DOWN:
 			result = do_sem_down(&msg);
 			break;
-		case SEMA_INIT:
+		case SEM_INIT:
 			result = do_sem_init(&msg);
 			break;
 		case SEM_RELEASE:
