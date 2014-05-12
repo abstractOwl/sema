8f3213ef28bf03ef3e9ffc3a750268ad6065d5dc
Andrew Lo <andrewlo@umail.ucsb.edu>
2014-05-12 15:08:24 -0700

    Forgot to commit change

--- src/include/sema.h
+++ src/include/sema.h
@@ -4,6 +4,6 @@
 int sem_init    (int start_value);
 int sem_down    (int semaphore_number);
 int sem_up      (int semaphore_number);
-int sem_release (int semaphore_number)
+int sem_release (int semaphore_number);
 
 #endif /* _MINIX_SEMA_H */
